42185a289f5e715c5d14851cb952c50f10008963
Abdullah Zafar <abdullah.zafar@directfn.com>
2026-02-09 01:57:04 +0500

    Register function added

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -53,6 +53,19 @@ export const AuthProvider = ({ children }) => {
     throw new Error(res.data.msg || "Login failed");
   };
 
+  const register = async (data) => {
+    const res = await API.post("/register", data);
+    if (res.data.access_token) {
+      localStorage.setItem("firstLogin", "true");
+      localStorage.setItem("token", res.data.access_token);
+      localStorage.setItem("user", JSON.stringify(res.data.user));
+      setToken(res.data.access_token);
+      setUser(res.data.user);
+      return res.data;
+    }
+    throw new Error(res.data.msg || "Registration failed");
+  };
+
   const logout = () => {
     localStorage.removeItem("firstLogin");
     localStorage.removeItem("token");
@@ -60,10 +73,13 @@ export const AuthProvider = ({ children }) => {
     setToken(null);
     setUser(null);
     API.post("/logout");
+    window.location.href = "/login";
   };
 
   return (
-    <AuthContext.Provider value={{ user, token, loading, login, logout }}>
+    <AuthContext.Provider
+      value={{ user, token, loading, login, logout, register }}
+    >
       {children}
     </AuthContext.Provider>
   );
